bf238d11b0acb07df6e819b451c21ab521d8c57e
Michele Gramazio <gramaziom@tutanota.com>
2022-11-10 10:01:20 +0100

    Fix ctegories nel seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $ctegories = [
+        $categories = [
             ['name'=>'Supernova'],
             ['name'=>'Nana bianca'],
             ['name'=>'Nana nera'],
