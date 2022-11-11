fc5d6dcd4756e4abd2d6cd5d03746ca78dbad65b
Michele Gramazio <gramaziom@tutanota.com>
2022-11-11 14:56:03 +0100

    inizio fix searchbar

--- app/Models/Announce.php
+++ app/Models/Announce.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use App\Models\User;
 use App\Models\Category;
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Announce extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
     protected $fillable = [
         'name', 
         'price',
