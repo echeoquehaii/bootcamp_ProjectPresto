c4b592bd765c067b9c8e9894fda16502cad8c031
Michele Gramazio <gramaziom@tutanota.com>
2022-11-12 19:45:29 +0100

    Sistemate card

--- resources/views/announces/categoryShow.blade.php
+++ resources/views/announces/categoryShow.blade.php
@@ -4,42 +4,51 @@
     <h1 class="text-center mt-custom">Ecco la tua categoria stellare: {{$category->name}}</h1>
 
     
-    <div class="container height">
-        <div class="row">
+    <div class="container-fluid height">
+        <div class="row mt-custom">
             @forelse ($acceptedAnnounces as $announce)
-               <div class="card0">
-            <div class="lines"></div>
-            <div class="imgBx">
-                <img src="https://picsum.photos/200/300" alt="" class="imageBx">
-            </div>
-            <div class="content">
-                <div class="details">
-                    <h2>{{$announce->name}}</h2>
-                    <div class="data">
-                        <h3>{{$announce->user->name}}</h3>
-                        <h3>{{$announce->category->name}}</h3>
+            <div class="col-12 col-md-4 mt-custom mx-0 card-height d-flex justify-content-center">
+                <div class="card0">
+                    <div class="lines"></div>
+                    <div class="imgBx pb-0">
+                        <img src="https://picsum.photos/200/300" alt="" class="imageBx pb-0">
                     </div>
-                    <div class="actionBtn">
-                        <a href="{{route('detAnnounce', compact('announce'))}}"><button>Vai al dettaglio</button></a>
+                    <div class="content pb-2 pt-0">
+                        <div class="details pb-2 pt-0">
+                            <h2 class="cardTitle pb-4">{{$announce->name}}</h2>
+                            <div class="data pb-2">
+                                <h3>Venditore: {{$announce->user->name}}</h3>
+                                <h3>Categoria: {{$announce->category->name}}</h3>
+                            </div>
+                            <div class="actionBtn pb-2">
+                                <a href="{{route('detAnnounce', compact('announce'))}}"><button>Vai al dettaglio</button></a>
+                            </div>
+                        </div>
+        
                     </div>
-                </div>
-
+                    
+                </div> 
             </div>
-            
-         </div> 
             @empty
                 <p>Non ci sono oggetti di questa categoria!</p>
-            @auth
-             <div class="container height">
-                 <a href="{{route('createAnnounce')}}"><button class="btn btn-warning">Inserisci annuncio</button></a>
-             </div>
-            @endauth 
-        @endforelse
+                @auth
+                    <div class="container height">
+                        <a href="{{route('createAnnounce')}}"><button class="btn btn-warning">Inserisci annuncio</button></a>
+                    </div>
+                @endauth 
+            @endforelse
         
-         </div>
+         </div> 
+    </div>
 
-         
-    </div>     
+{{--     <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-4 d-flex justify-content-center mt-3">
+                {{$acceptedAnnounces->links()}}
+            </div>
+        </div>
+    </div> --}}
+
 
 
 </x-layout>
--- resources/views/announces/indexAnnounce.blade.php
+++ resources/views/announces/indexAnnounce.blade.php
@@ -7,28 +7,25 @@
             {{ session('message') }}
         </div>
     @endif
-
-    
-
    
-    <div class="container height">
+    <div class="container-fluid height">
         <div class="row mt-custom">
             @forelse ($announces as $announce)
             
-            <div class="col-12 col-md-4 mt-custom">
+            <div class="col-12 col-md-4 mt-custom mx-0 card-height d-flex justify-content-center">
                 <div class="card0">
                     <div class="lines"></div>
-                    <div class="imgBx">
-                        <img src="https://picsum.photos/200/300" alt="" class="imageBx">
+                    <div class="imgBx pb-0">
+                        <img src="https://picsum.photos/200/300" alt="" class="imageBx pb-0">
                     </div>
-                    <div class="content">
-                        <div class="details">
-                            <h2>{{$announce->name}}</h2>
-                            <div class="data">
+                    <div class="content pb-2 pt-0">
+                        <div class="details pb-2 pt-0">
+                            <h2 class="cardTitle pb-4">{{$announce->name}}</h2>
+                            <div class="data pb-2">
                                 <h3>Venditore: {{$announce->user->name}}</h3>
                                 <h3>Categoria: {{$announce->category->name}}</h3>
                             </div>
-                            <div class="actionBtn">
+                            <div class="actionBtn pb-2">
                                 <a href="{{route('detAnnounce', compact('announce'))}}"><button>Vai al dettaglio</button></a>
                             </div>
                         </div>
@@ -49,7 +46,7 @@
 
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-4 d-flex justify-content-center">
+            <div class="col-4 d-flex justify-content-center mt-3">
                 {{$announces->links()}}
             </div>
         </div>
