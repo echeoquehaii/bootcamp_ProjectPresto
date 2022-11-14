e14d5a6cfaf6af5fa60d70eadfd7ecb2c96a987b
Michele Gramazio <gramaziom@tutanota.com>
2022-11-14 17:14:49 +0100

    Creata logica form profilo per revisore con relativi errori e sistema anti spam

--- resources/views/livewire/profile-form.blade.php
+++ resources/views/livewire/profile-form.blade.php
@@ -12,26 +12,25 @@
                                 <h2 class="text-wh text-center mb-4">Inserisci le tue informazioni per diventare revisore</h2>
                         
                                 <div class="mb-4 inputBox">
-                                    <input type="numeric"  wire:model.lazy="age">
+                                    <input type="numeric" class="@error('age') is-invalid @enderror" wire:model.lazy="age">
                                     <label class="form-label">Età</label>
                                     <i></i>
                                 </div>
-                                @error('age') <span class="error text-wh">{{ $message }}</span> @enderror
+                                @error('age') <div class="error text-wh">{{ $message }}</div> @enderror
 
-                            
                                 <div class="mb-2 inputBox">
-                                    <input type="text"  wire:model.lazy="address">
+                                    <input type="text" class="@error('address') is-invalid @enderror" wire:model.lazy="address">
                                     <label class="form-label">Indirizzo</label>
                                     <i></i>
                                 </div>
-                                @error('address') <span class="error text-wh">{{ $message }}</span> @enderror
+                                @error('address') <div class="error text-wh">{{ $message }}</div> @enderror
 
                                 <div class="mb-2 inputBox">
-                                    <input type="text"  wire:model.lazy="description">
+                                    <input type="text" class="@error('description') is-invalid @enderror" wire:model.lazy="description">
                                     <label class="form-label">Descrizione</label>
                                     <i></i>
                                 </div>
-                                @error('description') <span class="error text-wh">{{ $message }}</span> @enderror
+                                @error('description') <div class="error text-wh">{{ $message }}</div> @enderror
                             
                                 <button type="submit" class="btnLogForm">Diventa revisore!</button>
                             </form>
