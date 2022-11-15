d13582923772789338773cfb44df6e36e4edf16b
Michele De Girolamo <degirolamo.michele02@gmail.com>
2022-11-15 19:06:06 +0100

    fix del merge

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,23 +13,23 @@
         </li>
         @guest
         <li class="nav-item">
-          <a class="nav-link text-wh hover-underline-animation" href="{{route('register')}}">Registrazione</a>
+          <a class="nav-link text-wh hover-underline-animation" href="{{route('register')}}">{{__('ui.registrazione')}}</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-wh hover-underline-animation" href="{{route('login')}}">Log In</a>
+          <a class="nav-link text-wh hover-underline-animation" href="{{route('login')}}">{{__('ui.login')}}</a>
         </li>
         @else
         
         <li class="nav-item">
-          <a class="nav-link text-wh hover-underline-animation" href="{{route('createAnnounce')}}">Crea Annuncio</a>
+          <a class="nav-link text-wh hover-underline-animation" href="{{route('createAnnounce')}}">{{__('ui.creaAnnuncio')}}</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-wh hover-underline-animation" href="{{route('indexAnnounce')}}">Lista Annunci</a>
+          <a class="nav-link text-wh hover-underline-animation" href="{{route('indexAnnounce')}}">{{__('ui.listaAnnunci')}}</a>
         </li>
         <li>
           <div class="nav-item dropdown hover-underline-animation">
               <p class="nav-link dropdown-toggle text-wh mb-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Categorie
+                {{__('ui.categorie')}}
               </p>
               <ul class="dropdown-menu">
                   @foreach ($categories as $category)
@@ -44,7 +44,7 @@
             <div class="searchBox">
               <div class="search"><i class="fa-solid fa-magnifying-glass"></i></div>
               <div class="searchInput">
-                <input name="searched" type="text" placeholder="Cerca" aria-label="Search" class="searchBoxInputField">
+                <input name="searched" type="text" placeholder="{{__('ui.cerca')}}" aria-label="Search" class="searchBoxInputField">
               </div>
               <div class="close"><i class="fa-sharp fa-solid fa-xmark"></i></div>
             </div>           
@@ -54,7 +54,7 @@
         <li>
           <div class="btn-group nav-item hover-underline-animation text-wh">
             <p class="nav-link dropdown-toggle text-wh mb-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Lingua
+              {{__('ui.lingua')}}
             </p>
             <ul class="dropdown-menu">
               <li class="nav-item">
@@ -78,16 +78,16 @@
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><span class="hover-underline-animation text-white">{{Auth::user()->name}}</span></a>
               <div class="dropdown-menu dropdown-menu-end" id="userdropjs">
                 <li>
-                  <a href="#" class="hover-underline-animation dropdown-item userdropjs-item text-white">Profile</a>
+                  <a href="#" class="hover-underline-animation dropdown-item userdropjs-item text-white">{{__('ui.profilo')}}</a>
                 </li>
                 @if(Auth::user()->is_revisor)
                   <li> 
-                    <a href="{{route('indexRevisor')}}" id="noShow" class="hover-underline-animation dropdown-item userdropjs-item text-white">Zona revisore: <span>{{App\Models\Announce::toBeRevisionedCount()}}
+                    <a href="{{route('indexRevisor')}}" id="noShow" class="hover-underline-animation dropdown-item userdropjs-item text-white">{{__('ui.zonaRevisore')}} <span>{{App\Models\Announce::toBeRevisionedCount()}}
                       <span class="visually-hidden">unread messages</span>
                     </span></a>
                   </li>             
                 @endif
-                <li><a class="me-5 hover-underline-animation dropdown-item userdropjs-item text-white" href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
+                <li><a class="me-5 hover-underline-animation dropdown-item userdropjs-item text-white" href="{{route('logout')}}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{__('ui.logout')}}</a>
                   <form id="logout-form" method="POST" class="d-none" action="{{route('logout')}}">
                     @csrf
                   </form>
