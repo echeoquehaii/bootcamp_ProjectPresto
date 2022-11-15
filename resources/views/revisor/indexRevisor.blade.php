4202730e750f139091f087d8eea6efc00817ae0b
dario sorrentino <d.sorrentino95@gmail.com>
2022-11-15 13:01:18 +0100

    finita pagina revisore, modificato effetto cards

--- resources/views/revisor/indexRevisor.blade.php
+++ resources/views/revisor/indexRevisor.blade.php
@@ -40,36 +40,41 @@
                 </div>
                    
                     <div class="col-12 col-md-4">
-                        <div class="text-center detText">
-                            <h5 class="card-title">Nome: {{$announce_to_check->name}}</h5>
-                            <p class="card-text">Venditore: {{$announce_to_check->user->name}}</p>
-                            <p class="card-text">Prezzo: {{$announce_to_check->price}} €</p>
-                            <p class="card-text">Posizione: {{$announce_to_check->location}}</p>
-                            <p class="card-text">Descrizione: {{$announce_to_check->description}}</p>
-                            <p class="card-text">Categoria: {{$announce_to_check->category->name}}</p>
+                        <div class="cardRev">       
+                            <div class="linesRev"></div>
+                                <div class="contentRev">
+                                    <div class="detText text-white">
+                                        <p class="card-text"><span class="fontRev">Nome:</span> {{$announce_to_check->name}}</p>
+                                        <p class="card-text"><span class="fontRev">Venditore:</span> {{$announce_to_check->user->name}}</p>
+                                        <p class="card-text"><span class="fontRev">Prezzo:</span> {{$announce_to_check->price}} €</p>
+                                        <p class="card-text"><span class="fontRev">Posizione:</span> {{$announce_to_check->location}}</p>
+                                        <p class="card-text"><span class="fontRev">Categoria:</span> {{$announce_to_check->category->name}}</p>
+                                        <p class="card-text"><span class="fontRev">Descrizione:</span> {{$announce_to_check->description}}</p>                                      
+                                    </div>
+                                </div>
                         </div>
                     </div>
+
                     <div class="col-12 col-md-12 mt-custom1 d-flex justify-content-evenly">
                         <form action="{{route('revisor.acceptAnnounce', ['announce'=>$announce_to_check])}}" method="POST">
                             @csrf
-                                @method('PATCH')
-                                    <button type="submit" class="btn btn-customRevAcc">Accetta</button>
+                            @method('PATCH')
+                                <button type="submit" class="btn btn-customRevAcc">Accetta</button>
                         </form>
 
                         <form action="{{route('revisor.rejectAnnounce', ['announce'=>$announce_to_check])}}" method="POST">
                             @csrf
-                                @method('PATCH')
-                                    <button type="submit" class="btn btn-customRevDen">Rifiuta</button>
+                            @method('PATCH')
+                                <button type="submit" class="btn btn-customRevDen">Rifiuta</button>
                         </form>
-                    </div>
-            </div>
-                    
-        </div>     
+                    </div>                  
+            </div>     
     @endif
 
-    @if (session('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
     @endif
+        </div>
 </x-layout>
